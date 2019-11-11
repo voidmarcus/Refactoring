1ee5e0fb993da5a7277cde3c1f1455a779038421
Marcus V. Oliveira <marcusvoliveira@dcc.ufmg.br>
2019-11-10 22:40:05 -0300

    Commit 9

--- Customer.java
+++ Customer.java
@@ -16,6 +16,23 @@ public class Customer {
 		   return _name;
 	   };
 
+       public String htmlStatement() {
+            Enumeration rentals = _rentals.elements();
+            String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+            while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            // show figures for each rental
+            result += each.getMovie().getTitle()+ ": " +
+                        String.valueOf(each.getCharge()) + "<BR>\n";
+            }
+            
+            // add footer lines
+            result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+            result += "On this rental you earned <EM>" +
+                String.valueOf(getTotalFrequentRenterPoints()) +
+                "</EM> frequent renter points<P>";
+            return result;
+        }       
 
        int getFrequentRenterPoints()
        {
